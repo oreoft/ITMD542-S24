bb85eb132b7b66edd39ee02da73e5ff9b8c6fc38
zouyifan <ft1216@foxmail.com>
2024-03-23 10:19:27 -0500

    feat(lab-4): init

---
+++ lab-4/app.js
@@ -0,0 +1,53 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes');
+var usersRouter = require('./routes/users');
+var contactsRouter = require('./routes/contacts');
+var moviesRouter = require('./routes/movies');
+const {setupDatabase} = require("./src/database");
+
+var app = express();
+
+try {
+    setupDatabase();
+    console.log('Database setup completed.');
+} catch (error) {
+    console.error('Failed to setup database or start server:', error);
+}
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/contacts', contactsRouter);
+app.use('/movies', moviesRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
